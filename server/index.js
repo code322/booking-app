c56a1abef9ff89b05fad88d947ee18ab4e9533b4
Tcodingg <princetlive@yahoo.com>
2023-03-11 12:39:13 -0500

    install mysql, cors, and cookieparser and configure the database

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app = express();
-
-app.use(cors);
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:5000',
+  })
+);
 const PORT = process.env.PORT || 5000;
 
-app.get('/test', (req, res) => {
-  res.send('test');
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`running in port ${PORT}`));
