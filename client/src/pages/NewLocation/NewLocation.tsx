94942522390cb6347eb46afc271277eb0d371447
Tcodingg <princetlive@yahoo.com>
2023-03-14 11:20:28 -0400

    remove unused code

--- client/src/pages/NewLocation/NewLocation.tsx
+++ client/src/pages/NewLocation/NewLocation.tsx
@@ -105,58 +105,6 @@ function NewLocation() {
             setAddedPhots={setAddedPhots}
             addedPhotos={addedPhotos}
           />
-          {/* <>
-            <div className='flex gap-2'>
-              <Content label={'photos'} info='More =  better'>
-                <InputFields
-                  name='photo'
-                  handleChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    setPhotoLink(e.target.value)
-                  }
-                  placeholder='Add a photo link...'
-                  value={photoLink}
-                />
-              </Content>
-              <button
-                className='bg-custom-red rounded-md flex items-center gap-2 text-xs text-white self-end p-2 capitalize'
-                onClick={handleUploadPhoto}
-              >
-                <Icon icon='ic:twotone-cloud-upload' />
-                <span>add photo</span>
-              </button>
-            </div>
-
-            <div className='mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2'>
-              <label
-                className='border h-28 w-28  rounded-md text-gray-500 flex justify-center items-center gap-2 cursor-pointer'
-                htmlFor='photoInput'
-              >
-                <Icon icon='ic:twotone-cloud-upload' />
-                Upload
-                <input
-                  id='photoInput'
-                  className='hidden'
-                  type='file'
-                  accept='image/*'
-                  multiple
-                  onChange={handleUpload}
-                />
-              </label>
-
-              <>
-                {addedPhotos.length > 0 &&
-                  addedPhotos.map((links: any) => (
-                    <div>
-                      <img
-                        className='h-28 w-28 object-cover center object-center rounded-md'
-                        src={`${API_URL}/uploads/` + links}
-                        alt=''
-                      />
-                    </div>
-                  ))}
-              </>
-            </div>
-          </> */}
 
           {/* Description */}
           <Content label='description' info='add description of the place'>
