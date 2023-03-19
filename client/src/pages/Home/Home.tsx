9aed2bdbba188e3b36d48ecbea280b9c8bed2a47
Tcodingg <princetlive@yahoo.com>
2023-03-18 22:41:26 -0400

    add change the description style

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -20,7 +20,7 @@ function Home() {
 
   return (
     <Container>
-      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-8'>
         {allLocations &&
           allLocations.map((locations) => {
             let price = Number(locations.details.price).toLocaleString(
@@ -34,15 +34,15 @@ function Home() {
             );
 
             return (
-              <div className='rounded-md shadow-customShadow bg-gray-100 '>
+              <div>
                 <div className='relative'>
                   <Link to={`/location/${locations.id}`}>
                     <img
-                      className='bg-blue-200 aspect-square object-cover'
+                      className='bg-blue-200 aspect-square object-cover rounded-lg'
                       src={`${API_URL}/uploads/${locations.photos[0]}`}
                       alt='no_image'
                     />
-                    <div className='absolute bg-gradient-to-b from-transparent to-gray-700 opacity-60 h-full w-full top-0 left-0 '></div>
+                    <div className='absolute bg-gradient-to-b from-transparent to-gray-700 opacity-60 h-full w-full top-0 left-0 rounded-lg'></div>
                     <div className='absolute bottom-2 text-white left-1'>
                       <span className=' text-white text-xl font-semibold '>
                         {price}
@@ -51,25 +51,25 @@ function Home() {
                     </div>
                   </Link>
                 </div>
-                <div className='py-3 px-2 '>
-                  <h2 className='font-semibold text-xl capitalize text-gray-700'>
+                <div className='py-3 '>
+                  <h2 className='font-semibold text-base capitalize text-gray-700'>
                     {locations.details.title}
                   </h2>
-                  <div className='flex items-center'>
+                  <div className='flex items-center justify-start'>
                     <Icon
-                      className='text-gray-600 mr-1 text-xl'
+                      className='text-gray-600 mr-1 text-sm'
                       icon='material-symbols:location-on-outline'
                     />
-                    <h2 className='text-gray-600 capitalize'>
+                    <h2 className='text-gray-600 capitalize text-sm'>
                       {locations.details.address}
                     </h2>
                   </div>
-                  <div className='flex items-center'>
+                  <div className='flex items-center justify-start'>
                     <Icon
-                      className='text-gray-600 mr-1 text-xl'
+                      className='text-gray-600 mr-1 text-lg'
                       icon='mdi:bed-double-outline'
                     />
-                    <h2 className='text-gray-600 capitalize'>
+                    <h2 className='text-gray-600 text-sm'>
                       {locations.details.guests}
                     </h2>
                   </div>
