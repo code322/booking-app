36656949c4595d6146b655cf7a006ff8088579f3
Tcodingg <princetlive@yahoo.com>
2023-03-25 12:16:53 -0400

    add min height to the container component

--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -2,7 +2,11 @@ type Props = {
   children?: JSX.Element | JSX.Element[];
 };
 function Container({ children }: Props) {
-  return <section className='max-w-6xl m-auto px-3 py-4 '>{children}</section>;
+  return (
+    <section className='max-w-6xl m-auto px-3 py-4 min-h-screen'>
+      {children}
+    </section>
+  );
 }
 
 export default Container;
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -10,45 +10,43 @@ function Header() {
   const dispatch = useAppDispatch();
   console.log(user.name);
   return (
-    <header className='py-8 h-[3.5rem] border-b flex justify-center '>
-      <Container>
-        <div className='px-3  flex  justify-between h-full items-center w-full'>
-          {/* logo */}
-          <Link to={'/'} className='h-8 w-8 text-custom-red'>
-            <Icon className='h-10 w-10' icon='logos:airbnb-icon' />
+    <header className='py-8 h-[3.5rem] border-b flex justify-center m-auto '>
+      <div className='px-3  flex  justify-between h-full items-center w-full  max-w-6xl'>
+        {/* logo */}
+        <Link to={'/'} className='h-8 w-8 text-custom-red'>
+          <Icon className='h-10 w-10' icon='logos:airbnb-icon' />
+        </Link>
+        {/* nav */}
+        <div className='flex items-center gap-3'>
+          <Link
+            to={'/account'}
+            className='p-3 bg-custom-red text-white rounded-md outline-none flex items-center gap-2'
+          >
+            <span>My Listing</span>
           </Link>
-          {/* nav */}
-          <div className='flex items-center gap-3'>
-            <Link
-              to={'/account'}
-              className='p-3 bg-custom-red text-white rounded-md outline-none flex items-center gap-2'
-            >
-              <span>My Listing</span>
-            </Link>
-            <Link to={user.name ? '/' : '/login'}>
-              {user.name ? (
-                <>
-                  <Icon
-                    onClick={() => {
-                      IsLoggedLocalStorage.setIsLoggedInFalse();
-                      dispatch(logout() as any);
-                    }}
-                    className='text-gray-700'
-                    height={30}
-                    icon='la:sign-out-alt'
-                  />
-                </>
-              ) : (
+          <Link to={user.name ? '/' : '/login'}>
+            {user.name ? (
+              <>
                 <Icon
-                  className='text-gray-400'
+                  onClick={() => {
+                    IsLoggedLocalStorage.setIsLoggedInFalse();
+                    dispatch(logout() as any);
+                  }}
+                  className='text-gray-700'
                   height={30}
-                  icon='mdi:user-circle'
+                  icon='la:sign-out-alt'
                 />
-              )}
-            </Link>
-          </div>
+              </>
+            ) : (
+              <Icon
+                className='text-gray-400'
+                height={30}
+                icon='mdi:user-circle'
+              />
+            )}
+          </Link>
         </div>
-      </Container>
+      </div>
     </header>
   );
 }
