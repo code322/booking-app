1252dc486fb5fcffdc144608fc8448a2047ae7b2
Tcodingg <princetlive@yahoo.com>
2023-04-01 20:13:36 -0400

    remove unused code and add a min with to the account nav'

--- client/src/pages/Account/Account.tsx
+++ client/src/pages/Account/Account.tsx
@@ -1,18 +1,8 @@
-import React, { useState } from 'react';
-import Container from '../../components/Container/Container';
-import Locations from '../Locations/Locations';
 import {} from '../../state/locations/locationByIdSlicer';
 
-import EditLocation from '../EditLocation/EditLocation';
-import AddNewLocation from '../AddNewLocation/AddNewLocation';
 import { Link, useLocation } from 'react-router-dom';
 
 function Account() {
-  const [active, setActive] = useState<string>('profile');
-  const [showForm, setShowForm] = useState<boolean>(false);
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const [listId, setListId] = useState<number>(0);
-
   let inActiveNav = 'capitalize rounded-md p-2';
   let activeNav = 'capitalize rounded-md p-2 bg-custom-red text-white';
 
@@ -20,7 +10,7 @@ function Account() {
 
   return (
     <>
-      <div className='flex sm:flex-col gap-2'>
+      <div className='flex sm:flex-col gap-2 min-w-[100.7333px]'>
         <Link
           className={pathname === '/account' ? activeNav : inActiveNav}
           to='/account'
@@ -45,51 +35,6 @@ function Account() {
           add Listing
         </Link>
       </div>
-
-      {/* <div className='w-full flex gap-4 justify-center'>
-        <button
-          className={active === 'profile' ? activeNav : inActiveNav}
-          onClick={() => {
-            setActive('profile');
-            setEditMode(false);
-          }}
-        >
-          profile
-        </button>
-        <button
-          className={active === 'booking' ? activeNav : inActiveNav}
-          onClick={() => {
-            setActive('booking');
-            setEditMode(false);
-          }}
-        >
-          booking
-        </button>
-        <button
-          className={active === 'location' ? activeNav : inActiveNav}
-          onClick={() => {
-            setEditMode(false);
-            setActive('location');
-            setShowForm(false);
-          }}
-        >
-          Locations
-        </button>
-      </div> */}
-      {/* <>
-        {active === 'location' && editMode && showForm && <EditLocation />}
-        {active === 'location' && !editMode && showForm && <AddNewLocation />}
-        {active === 'location' && !showForm && (
-          <Locations
-            setShowForm={setShowForm}
-            setEditMode={setEditMode}
-            setListId={setListId}
-          />
-        )}
-
-        {active === 'profile' && 'profile'}
-        {active === 'booking' && 'booking'}
-      </> */}
     </>
   );
 }
