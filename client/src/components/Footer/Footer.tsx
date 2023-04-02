1416aeef0328769e781a46695b00bf34484ec875
Tcodingg <princetlive@yahoo.com>
2023-04-02 12:36:54 -0400

    refactor footer content

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,46 +5,90 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='py-4 px-3 mt-14 bg-custom-red min-h-[35vh] flex items-center'>
-      <div className='grid sm:grid-cols-4 sm:justify-items-center  align-middle max-w-6xl m-auto gap-2 sm:gap-0'>
-        <div></div>
-        <div className=''>
-          <h2 className='font-semibold text-white'>ABOUT US</h2>
-          <p className='text-xs mt-1 text-white '>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
+      <div className='grid sm:grid-cols-2 md:grid-cols-4 max-w-6xl m-auto gap-2 sm:gap-6 justify-between'>
+        <FooterContent
+          header='About Us'
+          children={
+            <>
+              <Content
+                info=' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
             vel tenetur blanditiis? Soluta quam est debitis, quae molestiae
             aliquid reiciendis. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Iure, obcaecati.
-          </p>
-        </div>
-        <div className=''>
-          <h2 className='font-semibold  text-white '>CUSTOMER SERVICE</h2>
-          <ul className='flex flex-col gap-2 mt-1'>
-            <li className='text-white text-xs'>Our Location</li>
-            <li className='text-white text-xs'>Terms and Conditions</li>
-            <li className='text-white text-xs'>Feedback</li>
-            <li className='text-white text-xs'>Contact Us</li>
-          </ul>
-        </div>
-        <div className=''>
-          <h2 className='font-semibold  text-white '>CONNECT WITH US</h2>
-          <ul className='flex  gap-2 mt-1'>
-            <li className='text-white text-xl'>
-              <Icon icon='ic:twotone-facebook' />
-            </li>
-            <li className='text-white text-xl'>
-              <Icon icon='ph:instagram-logo-fill' />
-            </li>
-            <li className='text-white text-xl'>
-              <Icon icon='mdi:linkedin' />
-            </li>
-            <li className='text-white text-xl'>
-              <Icon icon='ant-design:twitter-circle-filled' />
-            </li>
-          </ul>
-        </div>
+            adipisicing elit. Iure, obcaecati.'
+              />
+            </>
+          }
+        />
+        <FooterContent
+          header='CUSTOMER SERVICE'
+          children={
+            <>
+              <Content info='OUR LOCATION' />
+              <Content info='TERMS AND CONDITIONS' />
+              <Content info='FEEDBACK' />
+              <Content info='CONTACT US' />
+            </>
+          }
+        />
+
+        <FooterContent
+          header='MY PROFILE'
+          children={
+            <>
+              <Content info='MY ACCOUNT' />
+              <Content info='TERMS AND CONDITIONS' />
+              <Content info='SUPPORT' />
+              <Content info='HELP' />
+            </>
+          }
+        />
+        <FooterContent
+          header='CONNECT WITH US'
+          children={
+            <>
+              <Content
+                info={
+                  <div className='flex gap-3'>
+                    <Icon
+                      className='text-2xl hover:text-black cursor-pointer transition-all ease-out duration-200'
+                      icon='ic:twotone-facebook'
+                    />
+                    <Icon
+                      className='text-2xl hover:text-black cursor-pointer transition-all ease-out duration-200'
+                      icon='ph:instagram-logo-fill'
+                    />
+                    <Icon
+                      className='text-2xl hover:text-black cursor-pointer transition-all ease-out duration-200'
+                      icon='mdi:linkedin'
+                    />
+                    <Icon
+                      className='text-2xl hover:text-black cursor-pointer transition-all ease-out duration-200'
+                      icon='ant-design:twitter-circle-filled'
+                    />
+                  </div>
+                }
+              />
+            </>
+          }
+        />
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
+type FooterContentTypes = {
+  header: string;
+  children: JSX.Element | string;
+};
+const FooterContent = ({ header, children }: FooterContentTypes) => (
+  <div>
+    <h2 className='font-semibold  text-white '>{header}</h2>
+    <ul className='flex flex-col gap-2 mt-1'>{children}</ul>
+  </div>
+);
+
+const Content = ({ info }: { info: string | JSX.Element }) => (
+  <li className='text-white text-xs'>{info}</li>
+);
