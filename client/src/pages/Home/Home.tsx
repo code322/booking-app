b5e09a10d953a1862a9dfa359054f3efcc4d02ae
Tcodingg <princetlive@yahoo.com>
2023-04-18 14:33:13 -0400

    remove unused code

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import Container from '../../components/Container/Container';
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/useTypeSelector';
 import {
   getAllLocations,
@@ -8,10 +8,8 @@ import {
 import { API_URL } from '../../helpers/api';
 import { Link, useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
-import InputFields from '../../components/InputFields/InputFields';
 import { Slider } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material';
-import { filterLocations } from '../../helpers/filterLocations';
 import { getFilteredResult } from '../../state/locations/locationsSlicer';
 export type filterType = {
   query?: string;
@@ -23,6 +21,9 @@ export type filterType = {
 function Home() {
   const [search, setSearch] = useState('');
   const dispatch = useAppDispatch();
+  const [maxBed, setMaxBed] = useState<string>('Any');
+  const [princeRange, setPriceRange] = useState<number[]>([0, 200]);
+
   const theme = createTheme({
     palette: {
       primary: {
@@ -37,8 +38,6 @@ function Home() {
   const navigate = useNavigate();
 
   const allLocations = useAppSelector(selectLocations);
-
-  const [princeRange, setPriceRange] = useState<number[]>([0, 200]);
 
   // get the price range
   const minDistance = 10;
@@ -70,13 +69,6 @@ function Home() {
   function handleMaxBed(e: React.ChangeEvent<HTMLSelectElement>) {
     setMaxBed(e.target.value);
   }
-
-  const [maxBed, setMaxBed] = useState<string>('Any');
-
-  const filteredLocation = useMemo(() => {
-    let result = filterLocations(search, maxBed, princeRange, allLocations);
-    return result;
-  }, [search, maxBed, princeRange, allLocations]);
 
   function handleFilter(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
@@ -190,62 +182,61 @@ function Home() {
       </div>
       <Container>
         <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
-          {filteredLocation &&
-            filteredLocation.map((locations) => {
-              let price = Number(locations?.details?.price).toLocaleString(
-                'en-US',
-                {
-                  style: 'currency',
-                  currency: 'USD',
-                  maximumFractionDigits: 0,
-                  minimumFractionDigits: 0,
-                }
-              );
+          {allLocations?.map((locations) => {
+            let price = Number(locations?.details?.price).toLocaleString(
+              'en-US',
+              {
+                style: 'currency',
+                currency: 'USD',
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              }
+            );
 
-              return (
-                <div key={locations?.id}>
-                  <div className='relative'>
-                    <Link to={`/location/${locations?.id}`}>
-                      <img
-                        className='bg-blue-200 aspect-square object-cover rounded-lg w-full h-full'
-                        src={`${API_URL}/uploads/${locations?.photos?.[0]}`}
-                        alt='no_image'
-                      />
-                      <div className='absolute bg-gradient-to-b from-transparent to-gray-700 opacity-60 h-full w-full top-0 left-0 rounded-lg'></div>
-                      <div className='absolute bottom-2 text-white left-1'>
-                        <span className=' text-white text-xl font-semibold '>
-                          {price}
-                        </span>
-                        <span className='text-xs'>/night</span>
-                      </div>
-                    </Link>
-                  </div>
-                  <div className='py-3 '>
-                    <h2 className='font-semibold text-base capitalize text-gray-700 '>
-                      {locations?.details?.title}
+            return (
+              <div key={locations?.id}>
+                <div className='relative'>
+                  <Link to={`/location/${locations?.id}`}>
+                    <img
+                      className='bg-blue-200 aspect-square object-cover rounded-lg w-full h-full'
+                      src={`${API_URL}/uploads/${locations?.photos?.[0]}`}
+                      alt='no_image'
+                    />
+                    <div className='absolute bg-gradient-to-b from-transparent to-gray-700 opacity-60 h-full w-full top-0 left-0 rounded-lg'></div>
+                    <div className='absolute bottom-2 text-white left-1'>
+                      <span className=' text-white text-xl font-semibold '>
+                        {price}
+                      </span>
+                      <span className='text-xs'>/night</span>
+                    </div>
+                  </Link>
+                </div>
+                <div className='py-3 '>
+                  <h2 className='font-semibold text-base capitalize text-gray-700 '>
+                    {locations?.details?.title}
+                  </h2>
+                  <div className='flex items-center justify-start'>
+                    <Icon
+                      className='text-gray-600 mr-1 text-sm'
+                      icon='material-symbols:location-on-outline'
+                    />
+                    <h2 className='text-gray-600 capitalize text-xs '>
+                      {locations?.details?.address}
                     </h2>
-                    <div className='flex items-center justify-start'>
-                      <Icon
-                        className='text-gray-600 mr-1 text-sm'
-                        icon='material-symbols:location-on-outline'
-                      />
-                      <h2 className='text-gray-600 capitalize text-xs '>
-                        {locations?.details?.address}
-                      </h2>
-                    </div>
-                    <div className='flex items-center justify-start'>
-                      <Icon
-                        className='text-gray-600 mr-1 text-lg'
-                        icon='mdi:bed-double-outline'
-                      />
-                      <h2 className='text-gray-600 text-xs '>
-                        {locations?.details?.guests}
-                      </h2>
-                    </div>
+                  </div>
+                  <div className='flex items-center justify-start'>
+                    <Icon
+                      className='text-gray-600 mr-1 text-lg'
+                      icon='mdi:bed-double-outline'
+                    />
+                    <h2 className='text-gray-600 text-xs '>
+                      {locations?.details?.guests}
+                    </h2>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            );
+          })}
         </div>
       </Container>
     </>
